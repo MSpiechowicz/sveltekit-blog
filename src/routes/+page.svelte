6833f368548f3a755a4e1304d486ba255c3b5135
Maciej Spiechowicz <spm1fr@bosch.com>
2024-04-16 15:14:05 +0200

    lg view padding for the sky

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 
 <div class="flex h-[100%] flex-row justify-center align-middle">
 	<img
-		class="xl:top-42 absolute hidden w-[340px] sm:w-[620px] md:top-48 md:block md:w-[780px] lg:top-32 lg:w-[980px] xl:w-[1280px] 2xl:top-64 2xl:w-[1360px]"
+		class="xl:top-42 absolute hidden w-[340px] sm:w-[620px] md:top-48 md:block md:w-[780px] lg:top-32 lg:w-[980px] xl:w-[1280px] 2xl:top-64 2xl:w-[1360px] lg:px-14 2xl:px-6"
 		src={Sky}
 		alt="Sky"
 	/>
