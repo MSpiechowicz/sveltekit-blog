7d691b00bd67ecdbaf0e43976e90d44eb334c5bb
Maciej Spiechowicz <spm1fr@bosch.com>
2024-04-16 15:47:16 +0200

    close menu mobile before navigation change

--- src/lib/components/menu-hamburger.svelte
+++ src/lib/components/menu-hamburger.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
+  import { beforeNavigate } from '$app/navigation';
   import menuStore from '$lib/stores/menu-store.svelte';
   import { Hamburger } from 'svelte-hamburgers';
+
+  let open = menuStore.open;
+
+  beforeNavigate(() => {
+    open = false;
+    menuStore.toggle();
+  });
 </script>
 
 <div class="max-sm:-mr-3 sm:hidden">
-  <Hamburger on:click={menuStore.toggle} />
+  <Hamburger on:click={menuStore.toggle} bind:open />
 </div>
+
+open${menuStore.open}
