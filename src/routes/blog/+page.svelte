e09c52802eec271beb6d9803c12e8854c3f84081
Maciej Spiechowicz <spm1fr@bosch.com>
2024-04-16 16:01:31 +0200

    adjust mobile styling for the blog posts

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -11,19 +11,27 @@
 	<title>Maciej Spiechowicz - Blog posts</title>
 </svelte:head>
 
-<div class="flex h-[100%] flex-row justify-center items-center">
+<div class="flex h-[100%] flex-row items-center justify-center">
 	<ul class="flex flex-col gap-12">
 		{#each data.posts as post}
 			<li>
-        <div class="relative">
-          <h2 class="p-0 text-3xl sm:text-4xl md:text-5xl font-bold">{post.title}</h2>
-          <img src={Notes} alt="Notes" class="hidden sm:block w-[140px] h-auto absolute -top-4 -left-16" />
-        </div>
-				<p class="mt-2 text-xl sm:text-2xl">{post.description}</p>
-        <div class="flex flex-row gap-2 mt-4 items-center">
-          <p class="text:md sm:text-lg">{formatDate(post.date)}</p>
-          <Button class="p-[0.5rem] h-[26px] font-bold underline text:md sm:text-lg" variant="link" on:click={() => goto(`blog/${post.slug}`)}>Read full article</Button>
-        </div>
+				<div class="relative">
+					<h2 class="p-0 text-4xl md:text-5xl font-bold">{post.title}</h2>
+					<img
+						src={Notes}
+						alt="Notes"
+						class="absolute -left-16 -top-4 hidden h-auto w-[140px] sm:block"
+					/>
+				</div>
+				<p class="mt-2 text-xl lg:text-2xl">{post.description}</p>
+				<div class="mt-5 flex flex-row items-center gap-2">
+					<p class="text-lg">{formatDate(post.date)}</p>
+					<Button
+						class="text-lg h-[26px] p-[0.5rem] font-bold underline"
+						variant="link"
+						on:click={() => goto(`blog/${post.slug}`)}>Read full article</Button
+					>
+				</div>
 			</li>
 		{/each}
 	</ul>
