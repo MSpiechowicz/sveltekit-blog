8201f0606e54f54f9347998378661396c8e06e7c
Maciej Spiechowicz <spm1fr@bosch.com>
2024-04-16 14:47:53 +0200

    page transition

---
+++ src/lib/components/page-transition.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+
+	export let url: string;
+</script>
+
+{#key url}
+	<div class="h-[100%]" in:fade={{
+    delay: 0,
+    duration: 500,
+  }}>
+		<slot />
+	</div>
+{/key}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
-  import Footer from '$lib/components/footer.svelte';
-  import Navigation from '$lib/components/navigation.svelte';
-  import '../app.pcss';
+	import Footer from '$lib/components/footer.svelte';
+	import Navigation from '$lib/components/navigation.svelte';
+	import PageTransition from '$lib/components/page-transition.svelte';
+	import '../app.pcss';
+
+	export let data;
 </script>
 
 <div class="flex h-full w-full flex-col overflow-auto">
-	<header class="md:mx-18 relative mx-6 mt-4 sm:mx-12 lg:mx-18 xl:mx-20 2xl:mx-24">
-    <Navigation />
-  </header>
-  <main class="mx-8 mb-12 mt-8 flex-grow sm:mx-12 md:mx-12 lg:mx-14">
-    <slot />
-  </main>
+	<header class="md:mx-18 lg:mx-18 relative mx-6 mt-4 sm:mx-12 xl:mx-20 2xl:mx-24">
+		<Navigation />
+	</header>
+	<main class="mx-8 mb-12 mt-8 flex-grow sm:mx-12 md:mx-12 lg:mx-14">
+		<PageTransition url={data.url}>
+			<slot />
+		</PageTransition>
+	</main>
 	<Footer />
 </div>
