78762c01aefe3c27361441186193a3a32328240d
Maciej Spiechowicz <spm1fr@bosch.com>
2024-04-17 13:26:30 +0200

    update the rss.xml config

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -8,27 +8,28 @@ export async function GET({ fetch }) {
 	const headers = { 'Content-Type': 'application/xml' };
 
 	const xml = `
-		<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
-			<channel>
-				<title>${config.title}</title>
-				<description>${config.description}</description>
-				<link>${config.url}</link>
-				<atom:link href="${config.url}/rss.xml" rel="self" type="application/rss+xml"/>
-				${posts
-					.map(
-						(post) => `
-						<item>
-							<title>${post.title}</title>
-              <content type="html">${post.content}</content>
-							<link>${config.url}/blog/${post.slug}</link>
-							<guid isPermaLink="true">${config.url}/blog/${post.slug}</guid>
-							<pubDate>${new Date(post.date).toUTCString()}</pubDate>
-						</item>
-					`
-					)
-					.join('')}
-			</channel>
-		</rss>
+  <rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" version="2.0">
+    <channel>
+      <title>${config.title}</title>
+      <description>${config.description}</description>
+      <link>${config.url}</link>
+      <atom:link href="${config.url}/rss.xml" rel="self" type="application/rss+xml"/>
+      ${posts
+				.map(
+					(post) => `
+          <item>
+            <title><![CDATA[${post.title}]]></title>
+            <description><![CDATA[${post.description}]]></description>
+            <content:encoded><![CDATA[${post.content}]]></content:encoded>
+            <link>${config.url}/blog/${post.slug}</link>
+            <guid isPermaLink="true">${config.url}/blog/${post.slug}</guid>
+            <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+          </item>
+        `
+				)
+				.join('')}
+    </channel>
+  </rss>
 	`.trim();
 
 	return new Response(xml, { headers });
